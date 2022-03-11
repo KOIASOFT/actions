6b6c4ca1c5390941eb814ebf7d93a97d589f0708
Wojciech Szarmach <w.szarmach@koiasoft.no>
2022-03-11 15:04:19 +0100

    Move code of actions to separate scripts

---
+++ .scripts/docker-app-cfg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+test -n "$APP"         || { echo "Variable 'app' missing"; exit 1; }
+test -n "$CONFIG_PATH" || { echo "Variable 'config_path' missing"; exit 2; }
+test -n "$DEBUG"       || { echo "Variable 'debug' missing"; exit 3; }
+test -f "$CONFIG_PATH" || { echo "Config '$CONFIG_PATH' file not found"; exit 4; }
+
+alias yq='docker run --rm -v $PWD:/workdir mikefarah/yq'
+
+repository_query=".apps.${APP}.docker_repository"
+folder_query=".apps.${APP}.folder"
+
+export docker_image_tag=$(git rev-parse --short HEAD)
+export docker_registry=$(yq e '.apps.common.docker_registry' $CONFIG_PATH)
+export docker_repository_location_only=$(yq e "$repository_query" $CONFIG_PATH)
+export docker_repository="$docker_registry/$docker_repository_location_only"
+export docker_image="$docker_repository:$image_tag"
+export app_folder=$(yq e "$folder_query" $CONFIG_PATH)
+
---
+++ .scripts/list-available-apps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+test -n "$CONFIG_PATH"   || { echo "Variable 'config_path' missing"; exit 1; }
+test -n "$EXCLUDED_APPS" || { echo "Variable 'excluded_apps' missing"; exit 2; }
+test -n "$DEBUG"         || { echo "Variable 'debug' missing"; exit 3; }
+test -f "$CONFIG_PATH"   || { echo "Config '$CONFIG_PATH' file not found"; exit 4; }
+
+alias yq='docker run --rm -v $PWD:/workdir mikefarah/yq'
+
+test -f "$CONFIG_PATH" || { echo "Config '$CONFIG_PATH' not found"; exit 1; }
+
+apps_str=$(yq e '.apps | keys' $CONFIG_PATH)
+apps_list=$(sed 's/^- //' <<< "$apps_str" | sed -z 's/\n/,/g' | sed 's/,$//;s/ //g;s/#.*$//')
+
+IFS=',' read -r -a apps_array <<< "$apps_list"
+IFS=',' read -r -a excluded_array <<< "$EXCLUDED_APPS"
+
+apps=()
+
+for app in "${apps_array[@]}"; do
+      for excluded in "${excluded_array[@]}"; do
+          KEEP=true
+          if [[ "$app" == "$excluded" ]]; then
+              KEEP=false
+              break
+          fi
+      done
+
+      if ${KEEP}; then
+        apps+=($app)
+      fi
+done
+
+apps_csl=$(sed -z "s/ /,/g" <<< "${apps[@]}")
+apps_json=$(sed -z 's/\n/"]/;s/,/","/g;s/^/["/' <<< "$apps_csl")
