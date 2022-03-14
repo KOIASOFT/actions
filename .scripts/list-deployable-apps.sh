61b02aa167ca75e92213672fb7e32b3a7ec692f0
Wojciech Szarmach <w.szarmach@koiasoft.no>
2022-03-14 15:28:49 +0100

    Add multiple modules needed for deployment

--- .scripts/list-deployable-apps.sh
+++ .scripts/list-deployable-apps.sh
@@ -2,18 +2,21 @@
 shopt -s expand_aliases
 
 test -n "$CONFIG_PATH"   || { echo "Variable 'config_path' missing"; exit 1; }
-test -n "$EXCLUDED_APPS" || { echo "Variable 'excluded_apps' missing"; exit 2; }
-test -n "$DEBUG"         || { echo "Variable 'debug' missing"; exit 3; }
-test -f "$CONFIG_PATH"   || { echo "Config '$CONFIG_PATH' file not found"; exit 4; }
+test -n "$DEBUG"         || { echo "Variable 'debug' missing"; exit 2; }
+test -f "$CONFIG_PATH"   || { echo "Config '$CONFIG_PATH' file not found"; exit 3; }
+
+test -n "$APPS"          || { echo "Variable 'apps_input' missing"; exit 4; }
+test -n "$ACCOUNT"       || { echo "Variable 'account' missing"; exit 5; }
+test -n "$ENVIRONMENT"   || { echo "Variable 'environment' missing"; exit 6; }
 
 alias yq='docker run --rm -v $PWD:/workdir mikefarah/yq'
 
-echo "Check '$apps_input' apps in '$environment' environment of '$account' account"
+echo "Check '$APPS' apps in '$ENVIRONMENT' environment of '$ACCOUNT' account"
 
-if [ "$apps_input" == "all" ]; then
+if [ "$APPS" == "all" ]; then
   apps_str=$(yq e '.apps | keys' $CONFIG_PATH)
 else
-  apps_str="$apps_input"
+  apps_str="$APPS"
 fi
 
 apps_list=$(sed 's/^- //' <<< "$apps_str" | sed -z 's/\n/,/g' | sed 's/,$//;s/ //g;s/#.*$//')
@@ -23,9 +26,9 @@ allowed_apps=()
 
 for app in "${apps_array[@]}"; do
   if [ "$app" != "common" ]; then
-      available=$(yq e ".accounts.$account.apps.$app.environments | has(\"$environment\")" .infrastructure/config.yaml)
+      available=$(yq e ".accounts.$ACCOUNT.apps.$app.environments | has(\"$ENVIRONMENT\")" .infrastructure/config.yaml)
 
-      echo "App $app is allowed to be deployed in $environment: $available"
+      echo "App $app is allowed to be deployed in $ENVIRONMENT: $available"
 
       if [ "$available" == "true" ]; then
           allowed_apps+=("$app")
