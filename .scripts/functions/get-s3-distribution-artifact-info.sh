296a79f1a7db3bfcb40d83bfc59087d615b3d3ec
Wojciech Szarmach <w.szarmach@koiasoft.no>
2022-03-22 16:14:54 +0100

    Fix publish-s3-artifact.sh

--- .scripts/functions/get-s3-distribution-artifact-info.sh
+++ .scripts/functions/get-s3-distribution-artifact-info.sh
@@ -5,18 +5,15 @@ function get-s3-distribution-artifact-info() {
   local config_path
   local return_arr
   local app
-  local changeset
 
   return_arr="$1"
   config_path="$2"
   app="$3"
-  changeset="$4"
 
   test -n "$return_arr"        || { echo "Variable 'return_arr' missing";         exit 110; }
   test -n "$config_path"       || { echo "Variable 'config_path' missing";        exit 111; }
   test -f "$config_path"       || { echo "Config '$config_path' file not found";  exit 112; }
   test -n "$app"               || { echo "Variable 'app' missing";                exit 113; }
-  test -n "$changeset"         || { echo "Variable 'changeset' missing";          exit 114; }
 
   declare -n return="$return_arr"
 
@@ -27,7 +24,7 @@ function get-s3-distribution-artifact-info() {
 
   return["bucket"]=$(yq e "$bucket_query" "$config_path")
   return["name"]=$(yq e "$name_query" "$config_path")
-  return["changeset"]=$changeset
+  return["changeset"]=$(git rev-parse --short HEAD)
   return["destination"]="s3://${return["bucket"]}/${return["name"]}/${return["changeset"]}"
 
   test -n "${return["bucket"]}"       || { echo "Output 'bucket' missing";        exit 120; }
--- .scripts/publish-s3-artifact.sh
+++ .scripts/publish-s3-artifact.sh
@@ -6,7 +6,6 @@ test -n "$CONFIG_PATH"  || { echo "Variable 'config_path' missing"; exit 2; }
 test -n "$LOCAL_FOLDER" || { echo "Variable 'local_folder' missing"; exit 3; }
 test -n "$DEBUG"        || { echo "Variable 'debug' missing"; exit 4; }
 test -f "$CONFIG_PATH"  || { echo "Config '$CONFIG_PATH' file not found"; exit 5; }
-test -f "$CHANGESET"    || { echo "Variable 'changeset' missing"; exit 6; }
 
 source "$(dirname -- "${BASH_SOURCE[0]}")/functions/get-s3-distribution-artifact-info.sh"
 source "$(dirname -- "${BASH_SOURCE[0]}")/functions/synchronize-folder-with-s3-folder.sh"
