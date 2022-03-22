398917ab58f9018beb78551db3b1128c4e35ba8c
Wojciech Szarmach <w.szarmach@koiasoft.no>
2022-03-22 16:13:08 +0100

    Fix publish-s3-artifact.sh

--- .scripts/functions/get-s3-distribution-artifact-info.sh
+++ .scripts/functions/get-s3-distribution-artifact-info.sh
@@ -15,8 +15,8 @@ function get-s3-distribution-artifact-info() {
   test -n "$return_arr"        || { echo "Variable 'return_arr' missing";         exit 110; }
   test -n "$config_path"       || { echo "Variable 'config_path' missing";        exit 111; }
   test -f "$config_path"       || { echo "Config '$config_path' file not found";  exit 112; }
-  test -n "$app"               || { echo "Variable '$app' missing";               exit 113; }
-  test -n "$changeset"         || { echo "Variable '$changeset' missing";         exit 114; }
+  test -n "$app"               || { echo "Variable 'app' missing";                exit 113; }
+  test -n "$changeset"         || { echo "Variable 'changeset' missing";          exit 114; }
 
   declare -n return="$return_arr"
 
@@ -30,8 +30,8 @@ function get-s3-distribution-artifact-info() {
   return["changeset"]=$changeset
   return["destination"]="s3://${return["bucket"]}/${return["name"]}/${return["changeset"]}"
 
-  test -n "${return["bucket"]}"       || { echo "Output 'bucket' missing";      exit 120; }
-  test -n "${return["name"]}"         || { echo "Output 'name' missing";        exit 121; }
-  test -n "${return["changeset"]}"    || { echo "Output 'changeset' missing";   exit 122; }
-  test -n "${return["destination"]}"  || { echo "Output 'destination' missing"; exit 123; }
+  test -n "${return["bucket"]}"       || { echo "Output 'bucket' missing";        exit 120; }
+  test -n "${return["name"]}"         || { echo "Output 'name' missing";          exit 121; }
+  test -n "${return["changeset"]}"    || { echo "Output 'changeset' missing";     exit 122; }
+  test -n "${return["destination"]}"  || { echo "Output 'destination' missing";   exit 123; }
 }
